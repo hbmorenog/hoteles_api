cf64d9b7911fa606b3252007eef8350e4084fe06
hbmorenog <72283982+hbmorenog@users.noreply.github.com>
2020-12-17 23:35:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ api= FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-    "http://localhost", "http://localhost:8080",
+    "http://localhost", "http://localhost:8080", "https://https://g3m3-e9-hoteles-api.herokuapp.com/",
 ]
 
 api.add_middleware(
